416c3646c87652b9f785d46167152514bdca9702
onnkek <onnkek@gmail.com>
2024-06-14 10:56:30 +0500

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import './App.sass';
 import { Button } from 'components/Button';
+import { ReactComponent  as DownloadIcon } from 'assets/icons/download.svg';
 
 function App() {
   return (
-    <Button />
+    <div className='wrapper'>
+      <div>
+      <Button size='XS'><DownloadIcon />Label</Button>
+      <Button size='S' ><DownloadIcon />Label</Button>
+      <Button          ><DownloadIcon />Label</Button>
+      <Button size='L' ><DownloadIcon />Label</Button>
+      <Button size='XL'><DownloadIcon />Label</Button>
+      <Button size='XL' theme='outline'><DownloadIcon />Label</Button>
+      <Button size='XL' theme='color-outline'><DownloadIcon />Label</Button>
+      <Button size='XL' theme='clear'><DownloadIcon />Label</Button>
+      <Button size='XL' theme='color-clear'><DownloadIcon />Label</Button>
+
+        <Button disabled>Label</Button>
+        <Button >
+          <DownloadIcon />
+          Label
+        </Button>
+      </div>
+    </div>
   );
 }
 
--- src/components/Button/ui/Button.tsx
+++ src/components/Button/ui/Button.tsx
@@ -1,14 +1,48 @@
+import { Mods, classNames } from 'helpers/classNames';
 import cls from './Button.module.sass';
+import { Button as HButton } from '@headlessui/react'
+import { ReactNode } from 'react';
 
+type ButtonSize = 'XS' | 'S' | 'M' | 'L' | 'XL';
+type ButtonTheme = 'primary' | 'outline' | 'color-outline' | 'clear' | 'color-clear';
+const sizeClasses: Record<ButtonSize, string> = {
+  XS: cls.xs,
+  S: cls.s,
+  M: cls.m,
+  L: cls.l,
+  XL: cls.xl
+};
+const themeClasses: Record<ButtonTheme, string> = {
+  primary: cls.primary,
+  outline: cls.outline,
+  'color-outline': cls.colorOutline,
+  clear: cls.clear,
+  'color-clear': cls.colorClear,
+};
 export interface ButtonProps {
   className?: string;
+  children?: ReactNode;
+  disabled?: boolean;
+  size?: ButtonSize;
+  theme?: ButtonTheme;
 }
 
-export const Button = ({ className }: ButtonProps) => {
+export const Button = ({
+  className,
+  children,
+  disabled = false,
+  size = 'M',
+  theme = 'primary'
+}: ButtonProps) => {
 
+  const mods: Mods = {
+    [cls.disabled]: disabled,
+    [sizeClasses[size]]: true,
+    [themeClasses[theme]]: true
+  }
   return (
-    <div className={cls.button}>
-
-    </div>
+    <HButton disabled={disabled} className={classNames(cls.button, mods, [className])}>
+      {children}
+    </HButton>
   );
 };
